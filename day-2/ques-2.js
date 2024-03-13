f90b79de45411b65cdcdec4ebe6575eb7e2dece0
Ayush Pandey <ayush.pandey@ayushpandeyr-422.successivetech.com>
2024-03-13 16:05:17 +0530

    updated ques2 Asignment Day-2

--- day-2/ques-2.js
+++ day-2/ques-2.js
@@ -1,5 +1,5 @@
 // Problem Statement: Write a function to take input(age, name, phone no.) from user and display it
-const username="Ayush Pandey"
+const userName="Ayush Pandey"
 const age=21
-const phonenumber=9569452223
+const phoneNumber=9569452223
 console.log(`name: ${username}, age: ${age}, phonenumber: ${phonenumber}`)
