ac1cc016d785df93d60110851da98493fdbd12b0
Ayush Pandey <ayush.pandey@ayushpandeyr-422.successivetech.com>
2024-03-27 11:28:39 +0530

    changes based on review

--- day-4/ques-1.js
+++ day-4/ques-1.js
@@ -2,11 +2,11 @@
 
 const str = "Hello Yellow";
 const str1 = str.split("");
-//console.log(str1 );
 let str2 = [];
-for (let i = 0; i < str1.length; i++) {
-  if (str2.indexOf(str1[i]) == -1) {
-    str2.push(str1[i]);
+str1.map((ele)=>{
+  if (str2.indexOf(ele) == -1) {
+    str2.push(ele);
   }
-}
+})
+
 console.log(str2.join(""));
