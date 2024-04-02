f6683f3a4eb9d831420b69fb4723e4ddc4036efd
Ayush Pandey <ayush.pandey@ayushpandeyr-422.successivetech.com>
2024-04-02 12:29:44 +0530

    resolved comments

--- day-8/ques-1.js
+++ day-8/ques-1.js
@@ -6,7 +6,7 @@
 // }
 
 // Rectify Code :
-// when indexing sarts from 0 we have to iterate array untill the last element which is array.length-1 or in for loop we have to use only less than sign
+// when indexing sarts from 0 we have to iterate array until the last element which is array.length-1 or in for loop we have to use only less than sign
 let myArray = [1, 2, 3, 4];
 for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
